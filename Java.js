10379261e8fa8b375253917437f38f0fafe08362
Ak9312 <129245931+Ak9312@users.noreply.github.com>
2023-12-31 11:01:26 +0530

    Changing to let from var

--- Java.js
+++ Java.js
@@ -13,10 +13,10 @@ function myFunction() {
     alert('Please enter some text.');
     return;
   }
-  var table = document.getElementById("myTable");
-  var row = table.insertRow(-1);
-  var idCell = row.insertCell(0);
-  var textCell = row.insertCell(1);
+  let table = document.getElementById("myTable");
+  let row = table.insertRow(-1);
+  let idCell = row.insertCell(0);
+  let textCell = row.insertCell(1);
   //  let elem = Number(document.getElementById("Form 1").innerHTML);
   // elem = elem + 1;
   // cell1.innerHTML =  document.getElementById("From 1").innerHTML = String(elem);
